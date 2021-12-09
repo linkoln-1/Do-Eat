07a4eb08afac5aa6ef10ab2dd0064be5739aecce
alievhalid <s7600180@gmail.com>
2021-12-09 10:20:38 +0300

    two page succes

--- src/components/Dev_linkoln_Work/MainContent/RestaurantsNearby.jsx
+++ src/components/Dev_linkoln_Work/MainContent/RestaurantsNearby.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import "../scss/RestaurantsNearby.scss";
 function RestaurantsNearby(props) {
   return <div>ТУТ БУДЕТ КАРТА НА КОТОРОЙ ОТМЕЧЕНЫ РЕСТОРАНЫ ПО БЛИЗОСТИ</div>;
 }
--- src/components/Dev_linkoln_Work/MainContent/WhereWe.jsx
+++ src/components/Dev_linkoln_Work/MainContent/WhereWe.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-import "../scss/whereWe.scss.css";
+import "../scss/whereWe.scss";
 
 function WhereWe(props) {
-  return <div className="where__we">здесь будет адрес ресторана</div>;
+  return <div className="where__we">Ресторан находится по адресу г.Грозный, улица трошева 7</div>;
 }
 
 export default WhereWe;
